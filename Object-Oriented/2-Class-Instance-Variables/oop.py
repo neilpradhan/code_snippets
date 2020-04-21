70c37fdfba118b4e3bb355558b1deda16f50dee7
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-04-21 11:43:19 +0200

    Update oop.py

--- Object-Oriented/2-Class-Instance-Variables/oop.py
+++ Object-Oriented/2-Class-Instance-Variables/oop.py
@@ -1,14 +1,29 @@
 
 class Employee:
-
+    raise_amt = 1.04
+    num_of_emps = 0
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
+        Employee.num_of_emps +=1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    def apply_raise(self):
+        self.pay = self.pay * self.raise_amt
 
+        
+print("number_of_employees_before",Employee.num_of_emps)
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+print("number_of_employees_after",Employee.num_of_emps)
+emp_1.raise_amt = 5
+print(emp_1.raise_amt)
+print(Employee.raise_amt)
+
+
+print(emp_1.__dict__)
+print(Employee.__dict__)
+
